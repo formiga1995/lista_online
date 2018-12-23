1fe100a84803d217689efd3f4d496b952e645dd7
Gabriel Menezes Formiga <gmfformiga@gmail.com>
2018-12-23 21:34:54 -0200

    novos css

--- resources/views/landing-page-test.blade.php
+++ resources/views/landing-page-test.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-<title>Lista Online</title>
+<title>Bem vindo!</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="css/css_google.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
 <link rel="stylesheet" href="css/basic.css">
 <script src="js/basic.js"></script> 
 <body>
